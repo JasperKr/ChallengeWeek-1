d0922fc0ffeffd95ff6803f0268c1fea8af1ee0e
JasperKr <j.krocke@outlook.com>
2023-09-25 12:03:18 +0200

    travel to spaceport

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import random
+import time
 inventory = []
-speler_data = dict(name = "",surname = "")
+speler_data = dict(name = "")
 stories = dict(
-    intro = "You have landed on a spaceport and you are going to the sun. \n\
-The last thing you have to get is somewhere at the spaceport.\n\
-You have to get a sun suit. \n\
-There is a shoppingmall, a pilot walking around and you have a suitcase with you. "
+    intro = "You have arrived at the launchpad and you need to check in. There is a security guard. ",
+    departure = "you depart"
 )
-playing = True
-while playing:
-    print(stories["intro"])# begin het spel, print het begin verhaal text
-    name_correct = False
-    while not name_correct:
-        speler_data["name"] = input("What is your name? ").title()
-        speler_data["achternaam"] = input("What is your surname? ")
-        name_correct = input(f"Please verify your name, {speler_data['name']} {speler_data['achternaam']}, y/n: ") == "y"
-    
+def randomChance(chance):
+    return random.random() < (chance / 100)
+print(stories["intro"]) # begin het spel, print het begin verhaal text
+name_correct = False
+first_time_asking = True
+while not name_correct:
+    if first_time_asking:
+        speler_data["name"] = input("\"Hey! What's your name?\" ")
+    else:
+        speler_data["name"] = input("\"Can you say that again?\" ")
+    first_time_asking = False
+    name_correct = input(f"So, {speler_data['name']}? y/n: ") == "y"
+print(stories["departure"])
+print("travelling: ",end="",flush=True)
+for i in range(10):
+    time.sleep(0.5)
+    print(".",end=(i==9 and "\n" or ""),flush=True)
