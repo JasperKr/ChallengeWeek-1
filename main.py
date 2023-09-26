3d758beb675b55ef1cf8b7a9ebeabc547741a365
Jasper <102589741+JasperKr@users.noreply.github.com>
2023-09-26 11:45:30 +0200

    douane en timings; verhaal aangepast

--- main.py
+++ main.py
@@ -1,27 +1,27 @@
 import random
 import time
-inventory = dict(sun_suit = False)
+inventory = dict(moon_suit = False, nice_person = False)
 speler_data = dict(name = "")
 stories = dict(
     intro = "You have arrived at the launchpad. There is a security guard. ",
     departure_earth = "you depart",
-    arrival_spaceport = "You have landed on a spaceport and you are going to the sun. \n\
+    arrival_spaceport = "You have landed on a spaceport and you are going to the moon. \n\
 The last thing you have to get is somewhere at the spaceport.\n\
-You may have to get a sun suit. You may have forgotten it at home. \n\
+You may have forgotten your moon suit at home, but you're not sure.  \n\
 After looking around you can see a shoppingmall and what looks to be a pilot.\n\
 You only have your suitcase.",
-    approach_pilot_question = "Do you know where to buy a sun suit? I lost mine.",
-    approach_pilot_with_sunsuit = "\"I have a spare sun suit that you can borrow. \"",
-    pilot_has_no_extra_sunsuit = "\"you can buy a sunsuit at the shoppingmall. Be sure that you have the good shop. \"",
+    approach_pilot_question = "Do you know where to buy a moon suit? I lost mine.",
+    approach_pilot_with_moonsuit = "\"I have a spare moon suit that you can borrow. \"",
+    pilot_has_no_extra_moonsuit = "\"you can buy a moon suit at the shoppingmall. Be sure that you have the good shop. \"",
     go_to_store = "You go to the shoppingmall.",
     at_the_stores = "When you are at the shoppingmall, you can choose from 3 stores: Albert Hein, H&M and the Gilgal. \
-Wich one do you want to choose?",
+Which one do you want to go to?",
     Albert_Hein = "Did you also forget your orange juice?",
     H_M = "Nice suit for a fancy dress party.",
     Gilgal = "BIER!!!",
     go_to_gate = "You have everything you need, so you head over to gate ",
     you_help_someone = "You walk to the person who fell and you help him. He thanks you for your help. ",
-    you_do_not_help_someone = "You ignore him and you move on walking.",
+    you_do_not_help_someone = "You ignore him and you keep on walking.",
 
 )
 def random_chance(chance):
@@ -57,42 +57,42 @@ for i in range(10):
 print(stories["arrival_spaceport"],end="\n\n")
 time.sleep(5)
 
-# ask the player if they want to check their suitcase for a sun suit (15% chance it is there)
+# ask the player if they want to check their suitcase for a moon suit (15% chance it is there)
 if input("Do you want to check your suitcase first? y/n: ") == "y":
     time.sleep(2)
     if random_chance(15):
-        print("You had your sun suit with you all along.",end="\n\n")
-        inventory["sun_suit"] = True # keep track of if the player has a sun suit
+        print("You had your moon suit with you all along.",end="\n\n")
+        inventory["moon_suit"] = True # keep track of if the player has a moon suit
     else:
-        print("You forgot your sun suit.",end="\n\n")
+        print("You forgot your moon suit.",end="\n\n")
 
 time.sleep(2)
 
-if not inventory["sun_suit"]: # if the player didn't find a sun suit in their suitcase
+if not inventory["moon_suit"]: # if the player didn't find a moon suit in their suitcase
 
-    # ask the player if they want to ask the pilot where to buy a sun suit
-    if input("Do you want to ask the pilot where to buy a sun suit, or, go to the store to buy one? pilot/store: ") == "pilot":
+    # ask the player if they want to ask the pilot where to buy a moon suit
+    if input("Do you want to ask the pilot where to buy a moon suit, or, go to the store to buy one? pilot/store: ") == "pilot":
 
         print(stories["approach_pilot_question"]) # pilot storyline
         time.sleep(1)
 
         if random_chance(33):
-            # tell the player the pilot has a sun suit for them and add it to their inventory.
-            print(stories["approach_pilot_with_sunsuit"])
+            # tell the player the pilot has a moon suit for them and add it to their inventory.
+            print(stories["approach_pilot_with_moonsuit"])
             time.sleep(2)
-            inventory["sun_suit"] = True
+            inventory["moon_suit"] = True
         else:
-            # tell the player where to buy a sun suit.
-            print(stories["pilot_has_no_extra_sunsuit"])
+            # tell the player where to buy a moon suit.
+            print(stories["pilot_has_no_extra_moonsuit"])
             time.sleep(2)
-    if not inventory["sun_suit"]: #check if pilot gave you a sun suit, if not, go to the store.
-        # buy a sun suit
+    if not inventory["moon_suit"]: #check if pilot gave you a moon suit, if not, go to the store.
+        # buy a moon suit
         print(stories["go_to_store"])
         time.sleep(2)
         amount_visited = 0 # keep track of the stores the player has visited
         stores_visited = dict(Albert_Hein=False,H_M=False,Gilgal=False)
-        # if the player hasn't found a sunsuit yet.
-        while not inventory["sun_suit"]:
+        # if the player hasn't found a moon suit yet.
+        while not inventory["moon_suit"]:
             user_store_input = input("you have come to the shoppingmall. To which store do you want to go: Albert Hein, H&M, Gilgal? ").title()
             # the user input, Albert Hein, H&M, Gilgal or something invalid
             store_selection = ""
@@ -117,22 +117,36 @@ if not inventory["sun_suit"]: # if the player didn't find a sun suit in their su
                     time.sleep(2)
                     stores_visited[store_selection] = True
                     amount_visited += 1
-                    # 33% chance per store to have a sun suit, or, you checked two stores before this so it doesn't lock up the game.
+                    # 33% chance per store to have a moon suit, or, you checked two stores before this so it doesn't lock up the game.
                     if random_chance(33) or amount_visited >= 3:
-                        print("They had one more sunsuit")
-                        inventory["sun_suit"] = True
+                        print("They had one more moon suit")
+                        inventory["moon_suit"] = True
                     else:
-                        print("The store doesn't have any sunsuits")
+                        print("The store doesn't have any moon suits")
                 else:
                     print("You already went there!")
             else:
                 print("You can't find that store!")
 
 # the player goes to a gate
-print(stories["go_to_gate"]+random_from_list(["A", "B", "C", "E", "D", "F"])+".")
+gate = random_from_list(["A", "B", "C", "E", "D", "F"])
+print(stories["go_to_gate"]+gate+".")
 
-someone_falls = input("You see someone fall on your way to the gate, but you have haste. Will you help them? y/n: ")
+# does the player help someone
+someone_falls = input("You see someone fall on your way to the gate, but you're in a hurry. Will you still help them? y/n: ")
+time.sleep(3)
 if someone_falls == "y":
     print(stories["you_help_someone"])
+    time.sleep(3)
+    inventory["nice_person"] = True
 elif someone_falls == "n":
     print(stories["you_do_not_help_someone"])
+    time.sleep(3)
+
+# the player leaves the spaceport.
+print(f"You arrive at gate {gate}.",end="\n\n")
+time.sleep(1)
+print("Before entering you have to show your passport to the customs.")
+time.sleep(2)
+print(f"\"Thank you, have a nice flight {speler_data['name']}!\"")
+
