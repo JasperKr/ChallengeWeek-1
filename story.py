ecdb9b94dbae5a7b30de1b8faf73c39d7b04a6c3
Jasper <102589741+JasperKr@users.noreply.github.com>
2023-09-26 15:57:13 +0200

    verhaal naar story.py verplaatst

---
+++ story.py
@@ -0,0 +1,35 @@
+story = dict(
+    intro = "You have arrived at the launchpad. There is a security guard. ",
+    departure_earth = "you depart",
+    arrival_spaceport = "You have arrived at a spaceport and you are going to the moon. \n\
+You may have forgotten your moon suit at home, but you're not sure.  \n\
+After looking around you can see a shoppingmall and what looks to be a pilot.\n\
+You only have your suitcase and a pair of headphones.",
+    approach_pilot_question = "Do you know where to buy a moon suit? I lost mine.",
+    approach_pilot_with_moonsuit = "\"I have a spare moon suit that you can borrow. \"",
+    pilot_has_no_extra_moonsuit = "\"you can buy a moon suit at the shoppingmall. Be sure that you have the good shop. \"",
+    go_to_store = "You go to the shoppingmall.",
+    at_the_stores = "When you are at the shoppingmall, you can choose from 3 stores: Albert Hein, H&M and the Gilgal. \
+Which one do you want to go to?",
+    Albert_Hein = "Did you also forget your orange juice?",
+    H_M = "Nice suit for a fancy dress party.",
+    Gilgal = "BIER!!!",
+    buy_bottle = "Do you want to buy a bottle of Belgium wine? y/n: ",
+    go_to_gate = "You have everything you need, so you head over to gate ",
+    you_help_someone = "You walk to the person who fell and you help him. He thanks you for your help. ",
+    you_do_not_help_someone = "You ignore him and you keep on walking.",
+    arrival_spaceport_2 = "You arrived at the last spaceport before arriving at the moon. \n\
+This place looks shadey. ",
+    transfer_to_another_spaceship = "You have to wait another 8 hours before continuing your journey",
+    ask_to_explore_the_station = "Do you want to explore the station and find out what's there or not? y/n: ",
+    explore_the_station = "While exploring the station, you find a souvenirshop.",
+    get_robbed = "After you've bought a souvenir, you want to return to the station. \n\
+But while walking back, you see a man approaching you. \n\
+After he sees you looking around, he runs towards you and ",
+    nice_person_helps = "tries to rob you, he would have succeeded if not for the person you helped a while back.\n\
+You lost nothing and return safely.",
+    defend_with_bottle = "tries to rob you, he would have succeeded if not for that bottle you bought at Gilgal \n\
+You use it to defend yourself from the attacker but barely get away, you lost the bottle.",
+    get_robbed_without_help = "steals your headphones and souvenir",
+    sleep_safely_at_station = "You find a safe place somewhere in the station and wake up right before the flight leaves, time to get ready."
+)
