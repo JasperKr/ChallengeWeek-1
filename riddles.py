122a47ade1ea041abe0d51dd00c5b0d7539e5a3e
JasperKr <j.krocke@outlook.com>
2023-09-27 10:13:08 +0200

    mooooore riddles

--- riddles.py
+++ riddles.py
@@ -1,8 +1,14 @@
 riddles = [
     "The more you take, the more you leave behind. \"What am I?\"",
     "What has a head, a tail, is brown, and has no legs?",
+    "David's father has three sons: Snap, Crackle, and _____?",
+    "What belongs to you, but other people use it more than you?",
+    "Before Mount Everest was discovered, what was the highest mountain on Earth? "
 ]
 answers = [
     ["footsteps","footstep","step","steps"],
     ["penny","a penny", "pennies"],
+    ["david"],
+    ["name","your name",""],
+    ["mount everest"]
 ]
