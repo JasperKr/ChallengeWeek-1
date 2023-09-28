aacb4170b477ae8252deaa7c2f63658cfef57079
000Tiger0001 <1077100@hr.nl>
2023-09-28 15:07:19 +0200

    fixed story

--- story_2.py
+++ story_2.py
@@ -2,5 +2,6 @@ story_2 = dict(
     jeep = "After you rode hours in the jeep, the jeep stops and you can take a break. You step out of the jeep with your suit on. ",
     sand_or_not = "You see people taking some sand of mars with them. Do you also want sand of mars as a reminder of this trip?",
     continue_jeep = "After the break, you start riding the last part to the spaceport. ",
-    arrival_spaceport = "After you have traveled long, you arrive at the spaceport on mars. "
+    arrival_spaceport = "After you have traveled long, you arrive at the spaceport on mars. ",
+    
 )
